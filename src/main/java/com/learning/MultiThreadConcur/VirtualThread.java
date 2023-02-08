7680297b6092ad662b7d1bb7622b4612a4650bf8
PRAVUNITDGP <pravupravakarraopravu@outlook.com>
2023-02-08 06:02:06 +0530

    Changed

--- src/main/java/com/learning/MultiThreadConcur/VirtualThread.java
+++ src/main/java/com/learning/MultiThreadConcur/VirtualThread.java
@@ -1,15 +1,33 @@
 package com.learning.MultiThreadConcur;
 
+import java.awt.*;
+
 public class VirtualThread {
 
-    public static void main(String[] args) {
-        Runnable runnable = () -> {
-            for(int i=0 ; i < 100 ; i++){
-                System.out.println("Index is : " + i);
-            }
-        };
-
-    }
+//    public static void main(String[] args) {
+//        Runnable runnable = () -> {
+//            for(int i=0 ; i < 100 ; i++){
+//                System.out.println("Index is : " + i);
+//            }
+//        };
+//        Thread vThread1 = Thread.ofVirtual().start(runnable);
+//
+//        //Create but don't start virtual thread
+//        Thread vThreadUnstarted = Thread.ofVirtual().unstarted(runnable);
+//
+//        //Start manually
+//        vThreadUnstarted.start();
+//
+//        // Join the thread
+//        try{
+//            vThreadUnstarted.join();
+//        }catch (InterruptedException e){
+//            e.printStackTrace();
+//        }
+//
+//
+//
+//    }
 
 
 }
